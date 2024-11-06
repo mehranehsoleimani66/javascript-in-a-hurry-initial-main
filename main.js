acd71b508db6f9cc708098e3094fbb7a816002fc
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2024-11-06 16:44:53 +0330

    define local time dynamically

--- main.js
+++ main.js
@@ -10,13 +10,44 @@ document
   });
 // Greeting Section
 
+function selsucToFahr(temperature) {
+  let fahr = (temperature * 9) / 5 + 32;
+  return fahr;
+}
+
 const greetingText = "Morning";
 const weatherCondition = "sunny";
 const userLocation = "New York";
 let temperature = 22.78;
-let weatherText = `the weather is ${weatherCondition} in  ${userLocation} and it's ${temperature.toFixed(
+
+//alert("the temperaturre is " + selsucToFahr(temperature) + " F.");
+let fahrText = `the weather is ${weatherCondition} in  ${userLocation} and it's ${temperature.toFixed(
   1
 )} outside .`;
 
+let selsiusText = `the weather is ${weatherCondition} in  ${userLocation} and it's ${selsucToFahr(
+  temperature
+).toFixed(1)} outside .`;
+
 document.querySelector("#greeting").innerHTML = greetingText;
-document.querySelector("#weather").innerHTML = weatherText;
+document.querySelector("#weather").innerHTML = fahrText;
+
+document
+  .querySelector(".weather-group")
+  .addEventListener("click", function (e) {
+    console.log(e.target.id);
+    if (e.target.id === "fahr") {
+      document.querySelector("#weather").innerHTML = fahrText;
+    } else if (e.target.id === "celsius") {
+      document.querySelector("#weather").innerHTML = selsiusText;
+    }
+  });
+
+let localTime = new Date();
+
+document.querySelector("span[data-time=hours]").innerHTML =
+  localTime.getHours();
+document.querySelector("span[data-time=minutes]").innerHTML =
+  localTime.getMinutes();
+document.querySelector("span[data-time=seconds]").innerHTML =
+  localTime.getSeconds();
