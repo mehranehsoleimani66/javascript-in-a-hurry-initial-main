192626b63d9893b8046f3f1126269f0fc1fb1be4
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2024-11-06 15:18:38 +0330

    .

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Menu Section
+
 document.querySelector("#open-nav-menu").addEventListener("click", function () {
   document.querySelector("header nav .wrapper").classList.add("nav-open");
 });
@@ -6,3 +8,15 @@ document
   .addEventListener("click", function () {
     document.querySelector("header nav .wrapper").classList.remove("nav-open");
   });
+// Greeting Section
+
+const greetingText = "Morning";
+const weatherCondition = "sunny";
+const userLocation = "New York";
+let temperature = 22.78;
+let weatherText = `the weather is ${weatherCondition} in  ${userLocation} and it's ${temperature.toFixed(
+  1
+)} outside .`;
+
+document.querySelector("#greeting").innerHTML = greetingText;
+document.querySelector("#weather").innerHTML = weatherText;
